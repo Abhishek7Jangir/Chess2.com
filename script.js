3e50c3e6a9257aa1a6c36353b4a2c134c302a3ea
Abhishek7Jangir <abhishekjangir1234aj@gmail.com>
2025-03-28 23:16:20 +0530

    test

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ async function validateSignUp() {
     console.log('usernameValue', usernameValue);
     if (!isUsernameUnique || usernameValue === '') {
         nameDiv.style.border = '2px solid red';
-        isValid = false; // Mark validation as failed
+        isValid = false; // Mark validation as faile
     } else {
         nameDiv.style.border = '2px solid green';
     }
